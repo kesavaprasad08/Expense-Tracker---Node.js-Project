6e5d88bfbe42b39e560a09b8918d1da63d2e3564
kesavaprasad08 <kesav2661998@gmail.com>
2024-02-06 16:58:42 +0530

    Sending Reset Password Link

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const bodyParser = require("body-parser");
 
@@ -13,13 +13,14 @@ const sequelize = require("./util/database");
 const app = express();
 
 const userRoutes = require("./routes/user");
-const expenseRoutes = require('./routes/expense');
-const purchaseRoutes = require('./routes/purchase');
-const premiumRoutes =require('./routes/premium');
-const passwordRoutes = require('./routes/password');
+const expenseRoutes = require("./routes/expense");
+const purchaseRoutes = require("./routes/purchase");
+const premiumRoutes = require("./routes/premium");
+const passwordRoutes = require("./routes/password");
 
 const User = require("./models/user");
 const Expense = require("./models/expense");
+const ForgotPassword = require("./models/forgotPasswordRequests");
 
 app.use(bodyParser.json());
 app.use(express.json());
@@ -29,10 +30,10 @@ app.use(cors());
 
 app.use(express.static("public"));
 app.use("/user", userRoutes);
-app.use('/expense', expenseRoutes);
-app.use('/purchase', purchaseRoutes)
-app.use('/premium',premiumRoutes)
-app.use('/password',passwordRoutes)
+app.use("/expense", expenseRoutes);
+app.use("/purchase", purchaseRoutes);
+app.use("/premium", premiumRoutes);
+app.use("/password", passwordRoutes);
 app.use(homePage);
 
 User.hasMany(Expense);
